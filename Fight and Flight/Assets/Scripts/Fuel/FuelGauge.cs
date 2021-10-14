e39ed1427ff5a34a0c11eff023a17d3482a2e35d
Sam <samuelmcdouall@gmail.com>
2021-10-14 15:50:33 +0100

    Refactoring Fuel scripts

--- Fight and Flight/Assets/Scripts/Fuel/FuelGauge.cs
+++ Fight and Flight/Assets/Scripts/Fuel/FuelGauge.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FuelGauge : MonoBehaviour
 {
-
     public Slider gauge;
     public Gradient gradient;
     public Image fill;
--- Fight and Flight/Assets/Scripts/Fuel/FuelMeter.cs
+++ Fight and Flight/Assets/Scripts/Fuel/FuelMeter.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FuelMeter : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Material fuel_meter_m;
     public Color non_gliding_colour;
     public Color gliding_colour;
@@ -14,6 +11,4 @@ public class FuelMeter : MonoBehaviour
     public void SetNonGlidingColour() {
         fuel_meter_m.SetColor("_Color", non_gliding_colour);
     }
-
-
 }
