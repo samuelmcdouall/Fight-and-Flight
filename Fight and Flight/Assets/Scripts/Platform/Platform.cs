80acc42c077e9bf098a2dcf11bec1ddb3507cb12
Sam <samuelmcdouall@gmail.com>
2021-10-14 17:11:55 +0100

    Refactor Platform scripts

--- Fight and Flight/Assets/Scripts/Platform/Platform.cs
+++ Fight and Flight/Assets/Scripts/Platform/Platform.cs
@@ -1,10 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    // General
     public bool menu_platform = false;
+    [SerializeField]
+    bool starting_platform = false;
+
+    // Platform Lifetime
     bool deteriorating = false;
     float max_life_time = 10.0f;
     float current_life_time;
@@ -13,6 +16,11 @@ public class Platform : MonoBehaviour
     public Color color_max_life;
     public Color color_min_life;
     float color_lerp_time = 0;
+    bool begun_falling = false;
+    public AudioClip fall_sfx;
+    float platform_lifetime_difficulty_modifier = 2.0f;
+
+    // Platform Spawning
     GameObject platform_spawner;
     float y_out_of_bounds = 0.5f;
     [SerializeField]
@@ -20,19 +28,12 @@ public class Platform : MonoBehaviour
     [SerializeField]
     int percentage_chance_to_spawn_rare_pickup = 25;
     [SerializeField]
-    int percentage_chance_to_spawn_ammo_pickup = 20; // do all the stuff for an ammo pickup (including the prefab and the script)
+    int percentage_chance_to_spawn_ammo_pickup = 20;
     [SerializeField]
     float pickup_spawn_range_limit = 2.0f;
     public GameObject common_pickup;
     public GameObject rare_pickup;
     public GameObject ammo_pickup;
-    [SerializeField]
-    bool starting_platform = false;
-    bool begun_falling = false;
-    public AudioClip fall_sfx;
-
-
-    // Start is called before the first frame update
 
     void Start()
     {
@@ -40,80 +41,98 @@ public class Platform : MonoBehaviour
         {
             if (!starting_platform)
             {
-                int random_spawn_chance = Random.Range(1, 101);
-                if (random_spawn_chance < percentage_chance_to_spawn_common_pickup + 1)
-                {
-                    float pickup_random_x_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
-                    float pickup_random_z_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
-                    Vector3 pick_up_offset = transform.position + new Vector3(pickup_random_x_location, 1.0f, pickup_random_z_location);
-                    GameObject new_common_pickup = Instantiate(common_pickup, pick_up_offset, common_pickup.transform.rotation);
-                    new_common_pickup.transform.SetParent(gameObject.transform);
-
-                }
-                else if (random_spawn_chance < percentage_chance_to_spawn_common_pickup + percentage_chance_to_spawn_rare_pickup + 1)
-                {
-                    float pickup_random_x_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
-                    float pickup_random_z_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
-                    Vector3 pick_up_offset = transform.position + new Vector3(pickup_random_x_location, 1.0f, pickup_random_z_location);
-                    GameObject new_rare_pickup = Instantiate(rare_pickup, pick_up_offset, rare_pickup.transform.rotation);
-                    new_rare_pickup.transform.SetParent(gameObject.transform);
-                }
-                else if (random_spawn_chance < percentage_chance_to_spawn_common_pickup + percentage_chance_to_spawn_rare_pickup + percentage_chance_to_spawn_ammo_pickup + 1)
-                {
-                    float pickup_random_x_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
-                    float pickup_random_z_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
-                    Vector3 pick_up_offset = transform.position + new Vector3(pickup_random_x_location, 1.0f, pickup_random_z_location);
-                    GameObject new_ammo_pickup = Instantiate(ammo_pickup, pick_up_offset, ammo_pickup.transform.rotation);
-                    new_ammo_pickup.transform.SetParent(gameObject.transform);
-                }
+                DetermineIfPickUpSpawnedOnPlatform();
             }
-            max_life_time -= (2 * Player.player_level);
-            current_life_time = max_life_time;
-            platform_m = GetComponent<Renderer>().material;
-            platform_spawner = GameObject.FindGameObjectWithTag("Platform Spawner");
+            InitialPlatformSetup();
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!menu_platform)
         {
             if (transform.position.y <= y_out_of_bounds)
             {
-                platform_spawner.GetComponent<PlatformSpawner>().AttemptToSpawnPlatform();
-                Destroy(gameObject);
+                ReplacePlatform();
             }
             if (deteriorating)
             {
                 if (current_life_time > 0.0f)
                 {
                     current_life_time -= Time.deltaTime;
-                    platform_m.SetColor("_Color", Color.Lerp(color_max_life, color_min_life, color_lerp_time / max_life_time));
-                    color_lerp_time += Time.deltaTime;
+                    ChangePlatformColour();
                 }
-
                 else if (current_life_time < 0.0f)
                 {
                     current_life_time = 0.0f;
                 }
-
                 else
                 {
-                    transform.Translate(-Vector3.up * Time.deltaTime * platform_fall_speed);
-                    if (!begun_falling)
-                    {
-                        AudioSource.PlayClipAtPoint(fall_sfx, transform.position);
-                        begun_falling = true;
-                    }
+                    PlatformFalls();
                 }
             }
         }
     }
 
+    private void DetermineIfPickUpSpawnedOnPlatform()
+    {
+        int random_spawn_chance = Random.Range(1, 101);
+        if (random_spawn_chance < percentage_chance_to_spawn_common_pickup + 1)
+        {
+            SpawnPickUp(common_pickup);
+        }
+        else if (random_spawn_chance < percentage_chance_to_spawn_common_pickup + percentage_chance_to_spawn_rare_pickup + 1)
+        {
+            SpawnPickUp(rare_pickup);
+        }
+        else if (random_spawn_chance < percentage_chance_to_spawn_common_pickup + percentage_chance_to_spawn_rare_pickup + percentage_chance_to_spawn_ammo_pickup + 1)
+        {
+            SpawnPickUp(ammo_pickup);
+        }
+    }
+
+    private void SpawnPickUp(GameObject pickup)
+    {
+        float pickup_random_x_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
+        float pickup_random_z_location = Random.Range(-pickup_spawn_range_limit, pickup_spawn_range_limit);
+        Vector3 pick_up_offset = transform.position + new Vector3(pickup_random_x_location, 1.0f, pickup_random_z_location);
+        GameObject new_pickup = Instantiate(pickup, pick_up_offset, pickup.transform.rotation);
+        new_pickup.transform.SetParent(gameObject.transform);
+    }
+
+    private void InitialPlatformSetup()
+    {
+        max_life_time -= Player.player_level * platform_lifetime_difficulty_modifier;
+        current_life_time = max_life_time;
+        platform_m = GetComponent<Renderer>().material;
+        platform_spawner = GameObject.FindGameObjectWithTag("Platform Spawner");
+    }
+
+    private void ReplacePlatform()
+    {
+        platform_spawner.GetComponent<PlatformSpawner>().AttemptToSpawnPlatform();
+        Destroy(gameObject);
+    }
+
+    private void ChangePlatformColour()
+    {
+        platform_m.SetColor("_Color", Color.Lerp(color_max_life, color_min_life, color_lerp_time / max_life_time));
+        color_lerp_time += Time.deltaTime;
+    }
+
+    private void PlatformFalls()
+    {
+        transform.Translate(-Vector3.up * Time.deltaTime * platform_fall_speed);
+        if (!begun_falling)
+        {
+            AudioSource.PlayClipAtPoint(fall_sfx, transform.position);
+            begun_falling = true;
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "GroundCheck")
         {
             deteriorating = true;
         }
--- Fight and Flight/Assets/Scripts/Platform/PlatformSpawner.cs
+++ Fight and Flight/Assets/Scripts/Platform/PlatformSpawner.cs
@@ -1,14 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    // can increase these to increase difficulty
+    // General
     public GameObject platform;
-    [SerializeField]
-    int starting_platforms = 2;
+
+    // Spawning parameters
     [SerializeField]
     [Range(5.0f, 20.0f)]
     float maximum_horizontal_spawn_distance = 20.0f;
@@ -24,6 +21,9 @@ public class PlatformSpawner : MonoBehaviour
     [SerializeField]
     [Range(15.0f, 40.0f)]
     float maximum_spawn_platform_proximity_distance = 15.0f;
+    [SerializeField]
+    int starting_platforms = 2;
+
     void Start()
     {
         for(int platform = 0; platform < starting_platforms; platform++)
@@ -40,16 +40,32 @@ public class PlatformSpawner : MonoBehaviour
 
     bool SpawnPlatform()
     {
+        Vector3 random_chosen_new_starting_position = RandomlyDetermineSpawnPosition();
+        Vector3 random_chosen_y_independent_new_starting_position = new Vector3(random_chosen_new_starting_position.x, 0.0f, random_chosen_new_starting_position.z);
+
+        if (SpawnPositionAppropriate(random_chosen_y_independent_new_starting_position))
+        {
+            Instantiate(platform, random_chosen_new_starting_position, Quaternion.identity);
+            return true;
+        }
+        return false;
+    }
+
+    Vector3 RandomlyDetermineSpawnPosition()
+    {
         float random_chosen_x_position = Random.Range(-maximum_horizontal_spawn_distance, maximum_horizontal_spawn_distance);
         float random_chosen_y_position = Random.Range(minimum_vertical_spawn_distance, maximum_vertical_spawn_distance);
         float random_chosen_z_position = Random.Range(-maximum_horizontal_spawn_distance, maximum_horizontal_spawn_distance);
-        Vector3 random_chosen_new_starting_position = new Vector3(random_chosen_x_position, random_chosen_y_position, random_chosen_z_position);
-        Vector3 random_chosen_y_independent_new_starting_position = new Vector3(random_chosen_x_position, 0.0f, random_chosen_z_position);
+        return new Vector3(random_chosen_x_position, random_chosen_y_position, random_chosen_z_position);
+    }
 
+
+    bool SpawnPositionAppropriate(Vector3 random_chosen_y_independent_new_starting_position)
+    {
         bool spawned_close_enough_to_existing_platform = false;
         GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
 
-        foreach(GameObject platform in platforms)
+        foreach (GameObject platform in platforms)
         {
             Vector3 platform_y_independent_position = new Vector3(
                                                           platform.gameObject.transform.position.x,
@@ -67,7 +83,6 @@ public class PlatformSpawner : MonoBehaviour
         }
         if (spawned_close_enough_to_existing_platform)
         {
-            Instantiate(platform, random_chosen_new_starting_position, Quaternion.identity);
             return true;
         }
         return false;
