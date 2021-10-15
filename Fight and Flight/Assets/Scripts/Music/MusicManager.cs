75ac7394a24e69275d8278675449ce42271966e0
Sam <samuelmcdouall@gmail.com>
2021-10-15 22:01:39 +0100

    Update MusicManager.cs
    Updated for victory after beating boss

--- Fight and Flight/Assets/Scripts/Music/MusicManager.cs
+++ Fight and Flight/Assets/Scripts/Music/MusicManager.cs
@@ -1,22 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     public AudioClip boss_music;
     public AudioClip victory_music;
     AudioSource music_as;
     bool started_playing_boss_music = false;
+    bool started_playing_victory_music = false;
     void Start()
     {
         music_as = GetComponent<AudioSource>();
         music_as.playOnAwake = true;
         music_as.loop = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Player.score >= 50)
@@ -29,9 +25,16 @@ public class MusicManager : MonoBehaviour
                 music_as.Play();
                 started_playing_boss_music = true;
             }
-            else
+            if (Player.victory)
             {
-                // do stuff later for victory music (need to do the screen as well)
+                if (!started_playing_victory_music)
+                {
+                    music_as.Stop();
+                    music_as.clip = victory_music;
+                    music_as.loop = true;
+                    music_as.Play();
+                    started_playing_victory_music = true;
+                }
             }
         }
     }
