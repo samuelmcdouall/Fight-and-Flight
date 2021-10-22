63e142dfd487f636c32eefe15e6ac4d9b06ba71c
Sam <samuelmcdouall@gmail.com>
2021-10-22 17:44:20 +0100

    Don't display level during boss and and victory screen

--- Fight and Flight/Assets/Scripts/UI/ScoreUI.cs
+++ Fight and Flight/Assets/Scripts/UI/ScoreUI.cs
@@ -4,14 +4,23 @@ using UnityEngine.UI;
 public class ScoreUI : MonoBehaviour
 {
     Text score_text;
+    string current_level;
     void Start()
     {
         score_text = GetComponent<Text>();
     }
     void Update()
     {
+        if (Player.boss_spawned)
+        {
+            current_level = "BOSS";
+        }
+        else
+        {
+            current_level = "Level: " + (Player.player_level + 1);
+        }
         score_text.text = "Score: " + Player.score + "\n" +
-                          "Level: " + (Player.player_level + 1) + "\n" +
+                          current_level + "\n" +
                           "Drones : " + (Player.drones_destroyed);
     }
 }
--- Fight and Flight/Assets/Scripts/UI/VictoryUI.cs
+++ Fight and Flight/Assets/Scripts/UI/VictoryUI.cs
@@ -11,7 +11,6 @@ public class VictoryUI : MonoBehaviour
         gamer_over_text = GetComponent<Text>();
         gamer_over_text.text = "You win!" + "\n" + "\n" +
                                "Final Score: " + Player.score + "\n" +
-                               "Final Level: " + (Player.player_level + 1) + "\n" +
                                "Drones Destroyed: " + (Player.drones_destroyed);
     }
 }
