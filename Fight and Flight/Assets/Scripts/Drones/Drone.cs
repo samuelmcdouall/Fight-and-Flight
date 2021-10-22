4ca741364af45c94b53ce3aebb257bd73207dc53
Sam <samuelmcdouall@gmail.com>
2021-10-22 16:22:09 +0100

    Got rid of the Drone speed modifier as it wasn't being used
    it was 1.0f so it wasn't doing anything

--- Fight and Flight/Assets/Scripts/Drones/Drone.cs
+++ Fight and Flight/Assets/Scripts/Drones/Drone.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class Drone : DroneBase
 {
-    // General
-    float drone_speed_difficulty_modifier;
     
     void Start()
     {
@@ -16,6 +14,7 @@ public class Drone : DroneBase
         transform.LookAt(player_hit_box.transform);
         if (!menu_drone)
         {
+            drone_speed = Player.player_level;
             DetermineWaypointAndMove();
             DetermineIfTimeToFire();
         }
@@ -24,9 +23,10 @@ public class Drone : DroneBase
     public void InitialDroneSetup()
     {
         InitialBaseDroneSetup();
-        waypoint_targets.AddRange(drone_spawner.GetComponent<DroneSpawner>().waypoints);
-        drone_speed_difficulty_modifier = 1.0f;
-        drone_speed = Player.player_level * drone_speed_difficulty_modifier;
-        waypoint_determined = false;
+        if (!menu_drone)
+        {
+            waypoint_targets.AddRange(drone_spawner.GetComponent<DroneSpawner>().waypoints);
+            waypoint_determined = false;
+        }
     }
 }
