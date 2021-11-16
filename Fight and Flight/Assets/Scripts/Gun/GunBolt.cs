e8ae042e6a258a9ddb15e0fc9ce8b053b45b49ed
Sam <samuelmcdouall@gmail.com>
2021-11-16 15:36:56 +0000

    Switched if statements to switch case
    Inbuilt refactor tool does this

--- Fight and Flight/Assets/Scripts/Gun/GunBolt.cs
+++ Fight and Flight/Assets/Scripts/Gun/GunBolt.cs
@@ -45,76 +45,78 @@ public class GunBolt : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Platform")
+        switch (collider.gameObject.tag)
         {
-            Explode(explosion_fx);
-        }
-        else if (collider.gameObject.tag == "Target")
-        {
-            Destroy(collider.gameObject);
-            Explode(explosion_fx);
-        }
-        else if (collider.gameObject.tag == "Drone Hit Box")
-        {
-            Player.score += drone_value;
-            Player.drones_destroyed++;
-            Destroy(collider.gameObject.transform.parent.gameObject);
-            Explode(explosion_fx);
-        }
-        else if (collider.gameObject.tag == "Advanced Drone Hit Box")
-        {
-            if (collider.gameObject.transform.parent.gameObject.GetComponent<Drone>().current_drone_hp == 1)
-            {
-                Player.score += advanced_drone_value;
+            case "Platform":
+                Explode(explosion_fx);
+                break;
+
+            case "Target":
+                Destroy(collider.gameObject);
+                Explode(explosion_fx);
+                break;
+
+            case "Drone Hit Box":
+                Player.score += drone_value;
                 Player.drones_destroyed++;
                 Destroy(collider.gameObject.transform.parent.gameObject);
                 Explode(explosion_fx);
-            }
-            else
-            {
-                collider.gameObject.transform.parent.gameObject.GetComponent<Drone>().current_drone_hp--;
-                collider.gameObject.GetComponent<AdvancedDroneAnimation>().PlayDamageAnimation();
-                Explode(explosion_fx);
-            }
-        }
-        else if (collider.gameObject.tag == "Menu Drone Hit Box")
-        {
-            SceneManager.LoadScene("GameScene");
-        }
-        else if (collider.gameObject.tag == "Boss Drone Hit Box")
-        {
-            if (collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_boss_drone_hp == 1)
-            {
-                collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_boss_drone_hp--;
-                boss_healthbar.SetBossBar(collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_boss_drone_hp);
-                Player.score += boss_drone_value;
-                Player.drones_destroyed++;
-                Player.victory_countdown_begun = true;
-                DestroyAllCurrentBossRockets();
-                Destroy(collider.gameObject.transform.parent.gameObject);
-                Explode(boss_explosion_fx);
-            }
-            else
-            {
-                if (!collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().invulnerable)
+                break;
+
+            case "Advanced Drone Hit Box":
+                if (collider.gameObject.transform.parent.gameObject.GetComponent<Drone>().current_drone_hp == 1)
                 {
-                    collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_boss_drone_hp--;
-                    boss_healthbar.SetBossBar(collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_boss_drone_hp);
+                    Player.score += advanced_drone_value;
+                    Player.drones_destroyed++;
+                    Destroy(collider.gameObject.transform.parent.gameObject);
+                    Explode(explosion_fx);
                 }
-                Explode(explosion_fx);
-            }
-        }
-        else if (collider.gameObject.tag == "Volume Control")
-        {
-            collider.gameObject.GetComponent<VolumeControl>().ChangeVolume();
-            AudioSource.PlayClipAtPoint(explosion_sfx, player.transform.position, VolumeManager.sfx_volume);
-            Destroy(gameObject);
-        }
-        else if (collider.gameObject.tag == "Difficulty Setting")
-        {
-            collider.gameObject.GetComponent<DifficultySetting>().ChangeDifficulty();
-            AudioSource.PlayClipAtPoint(explosion_sfx, player.transform.position, VolumeManager.sfx_volume);
-            Destroy(gameObject);
+                else
+                {
+                    collider.gameObject.transform.parent.gameObject.GetComponent<Drone>().current_drone_hp--;
+                    collider.gameObject.GetComponent<AdvancedDroneAnimation>().PlayDamageAnimation();
+                    Explode(explosion_fx);
+                }
+                break;
+
+            case "Menu Drone Hit Box":
+                SceneManager.LoadScene("GameScene");
+                break;
+
+            case "Boss Drone Hit Box":
+                if (collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_drone_hp == 1)
+                {
+                    collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_drone_hp--;
+                    boss_healthbar.SetBossBar(collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_drone_hp);
+                    Player.score += boss_drone_value;
+                    Player.drones_destroyed++;
+                    Player.victory_countdown_begun = true;
+                    DestroyAllCurrentBossRockets();
+                    Destroy(collider.gameObject.transform.parent.gameObject);
+                    Explode(boss_explosion_fx);
+                }
+                else
+                {
+                    if (!collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().invulnerable)
+                    {
+                        collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_drone_hp--;
+                        boss_healthbar.SetBossBar(collider.gameObject.transform.parent.gameObject.GetComponent<BossDrone>().current_drone_hp);
+                    }
+                    Explode(explosion_fx);
+                }
+                break;
+
+            case "Volume Control":
+                collider.gameObject.GetComponent<VolumeControl>().ChangeVolume();
+                AudioSource.PlayClipAtPoint(explosion_sfx, player.transform.position, VolumeManager.sfx_volume);
+                Destroy(gameObject);
+                break;
+
+            case "Difficulty Setting":
+                collider.gameObject.GetComponent<DifficultySetting>().ChangeDifficulty();
+                AudioSource.PlayClipAtPoint(explosion_sfx, player.transform.position, VolumeManager.sfx_volume);
+                Destroy(gameObject);
+                break;
         }
     }
 
